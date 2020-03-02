58642e2d1e7a713212bea7a542cd7ef52544fee8
Ilya Yakavets <61687224+yakavetsiv@users.noreply.github.com>
2020-03-02 13:12:05 +0100

    Update FitModel11.m

--- FitModel11.m
+++ FitModel11.m
@@ -1,5 +1,8 @@
 function [k,go,out,xx,curve]=FitModel11(x,y)
-%задание граничный условий и параметров нелинейной регрессии
+% Setting the boundary conditions and nonlinear regression parameters
+% x - CD concentrations
+% y - Titration values
+
 fo_ = fitoptions('method','NonlinearLeastSquares','Robust','On',...
     'Lower',[1],'Upper',[Inf],'DiffMinChange',1e-03,...
     'TolFun',1e-012,'TolX',1e-11,'MaxIter',1e12);
@@ -8,24 +11,27 @@ if ~all( ok_ )
     warning( 'GenerateMFile:IgnoringNansAndInfs',...
         'Ignoring NaNs and Infs in data.' );
 end
-%начальные условия
+% Initial conditions
 st_ = [6e6];
 p0=0;
-%задание аппроксимационной функции
+% The setting of fitting fuction
 set(fo_,'Startpoint',st_);
 ft_ = fittype(@(k1,p0,x)ff11(k1,p0,x),...
     'problem',{'p0'},'dependent',{'y'},'independent',{'x'});
 
-%аппроксимация
+% Fitting
 [cf_,gof] = fit(x(ok_),y(ok_),ft_,fo_,'problem',{p0});
-%полученные параметры
+% Post-fitting statistical parameters
 par_=coeffvalues(cf_);
+% Binding constant
 k=[par_(1)];
+% Statistics
 go=[gof.adjrsquare,gof.rmse];
 ci = confint(cf_,0.95);
+% Formation of output array 
 out=[par_(1);gof.adjrsquare;gof.rmse;ci];
 
-%построение графика экспериментальных данных и полученной кривой
+% Plotting the fitting data
 h=semilogx(x,y);
 set(h,'Marker','.');
 axis([1e-9 1e-3 0 1]);
@@ -35,7 +41,7 @@ curve=ff11(k(1),p0,xx);
 plot(xx,curve,'-r');
 hold off;
 
-%модельная функция комплексообразования
+% Model fitting function (1:1 binding model)
 function y=ff11(k1,p0,xdata)
 p1=1;
 G0=xdata;
